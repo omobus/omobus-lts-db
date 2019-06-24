d22354a8984ca93b4773cc4a747a942fd8917fa2
Igor Artemov <i_artemov@omobus.net (zlts)>
2019-06-24 21:10:57 +0300

    added parsing data xconf's using unifdef.

--- _build/mssql/omobus-lts-db.sql
+++ _build/mssql/omobus-lts-db.sql
@@ -1777,6 +1777,6 @@ insert into sysparams(param_id, param_value, descr) values('db:vstamp', '', 'Dat
 go
 /* Copyright (c) 2006 - 2019 omobus-lts-db authors, see the included COPYRIGHT file. */
 
-update sysparams set param_value='3.4.22' where param_id='db:vstamp';
+update sysparams set param_value='3.4.23' where param_id='db:vstamp';
 
 go

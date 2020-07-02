4fa9cd1aab07a91925c96190b34070e6a3c32de4
Igor Artemov <i_artemov@omobus.net (zlts)>
2020-07-02 08:51:01 +0300

    3.5.1 release

--- _build/mssql/omobus-lts-db.sql
+++ _build/mssql/omobus-lts-db.sql
@@ -1906,6 +1906,6 @@ insert into sysparams(param_id, param_value, descr) values('db:vstamp', '', 'Dat
 go
 /* Copyright (c) 2006 - 2020 omobus-lts-db authors, see the included COPYRIGHT file. */
 
-update sysparams set param_value='3.5.0' where param_id='db:vstamp';
+update sysparams set param_value='3.5.1' where param_id='db:vstamp';
 
 go

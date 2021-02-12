cc2ffd15cd6da75d37274d4483bbc7a74d2ed6b0
Igor Artemov <i_artemov@omobus.net (zlts)>
2021-02-12 10:16:17 +0300

    data: removed [testing_*] and [dyn_testings] streams.

--- omobus-lts-db.sql
+++ omobus-lts-db.sql
@@ -1102,37 +1102,6 @@ create table target_types (
 create trigger trig_updated_ts before update on target_types for each row execute procedure tf_updated_ts();
 #endif //PGSQL
 
-create table testing_criterias (
-    db_id 		uid_t 		not null,
-    testing_criteria_id uid_t 		not null,
-    descr 		descr_t 	not null,
-    row_no 		int32_t 	null, -- ordering,
-    hidden 		bool_t 		not null default 0,
-    inserted_ts 	ts_auto_t 	not null,
-    updated_ts 		ts_auto_t 	not null,
-    primary key(db_id, testing_criteria_id)
-);
-
-#ifdef PGSQL
-create trigger trig_updated_ts before update on testing_criterias for each row execute procedure tf_updated_ts();
-#endif //PGSQL
-
-create table testing_scores (
-    db_id 		uid_t 		not null,
-    testing_score_id 	uid_t 		not null,
-    descr 		descr_t 	not null,
-    score 		int32_t 	not null /*check(score >= 0)*/,
-    row_no 		int32_t 	null, -- ordering,
-    hidden 		bool_t 		not null default 0,
-    inserted_ts 	ts_auto_t 	not null,
-    updated_ts 		ts_auto_t 	not null,
-    primary key(db_id, testing_score_id)
-);
-
-#ifdef PGSQL
-create trigger trig_updated_ts before update on testing_scores for each row execute procedure tf_updated_ts();
-#endif //PGSQL
-
 create table training_materials (
     db_id 		uid_t 		not null,
     tm_id 		uid_t 		not null,
@@ -1577,30 +1546,6 @@ create table dyn_stocks (
 
 #ifdef PGSQL
 create trigger trig_updated_ts before update on dyn_stocks for each row execute procedure tf_updated_ts();
-#endif //PGSQL
-
-create table dyn_testings (
-    db_id 		uid_t 		not null,
-    fix_date		date_t 		not null,
-    account_id 		uid_t 		not null,
-    contact_id 		uid_t 		not null,
-    testing_criteria_id uid_t 		not null,
-    testing_score_id 	uid_t 		null,
-    criteria_wf 	wf_t 		not null check(criteria_wf between 0.01 and 1.00),
-    score_wf 		wf_t 		null check(score_wf between 0.00 and 1.00),
-    score 		int32_t 	null check (score >= 0),
-    note 		note_t 		null,
-    sla 		numeric(6,5) 	not null check(sla between 0.0 and 1.0),
-    fix_dt		datetime_t 	not null,
-    user_id 		uid_t 		not null,
-    inserted_ts 	ts_auto_t 	not null,
-    updated_ts		ts_auto_t 	not null,
-    "_isRecentData"	bool_t 		null,
-    primary key(db_id, fix_date, account_id, contact_id, testing_criteria_id)
-);
-
-#ifdef PGSQL
-create trigger trig_updated_ts before update on dyn_testings for each row execute procedure tf_updated_ts();
 #endif //PGSQL
 
 create table holidays (

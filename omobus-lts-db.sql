95dcd97aed1396508b55ed63b4443235cf129c32
Igor Artemov <i_artemov@omobus.net (zlts)>
2022-11-28 12:47:04 +0300

    data: added [training_type_ids] attribute to the [training_materials] stream.

--- omobus-lts-db.sql
+++ omobus-lts-db.sql
@@ -1250,6 +1250,7 @@ create table training_materials (
     tm_id 		uid_t 		not null,
     descr 		descr_t 	not null,
     brand_ids 		uids_t 		null,
+    training_type_ids 	uids_t 		null,
     country_id 		country_t 	not null,
     dep_ids 		uids_t 		null,
     b_date 		date_t 		null,
